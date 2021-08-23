005f3d6318979c1dd5d41cc3d0ae5038dd094ec0
nabucodonosor-java <nabucodonosor.java@gmail.com>
2021-08-23 17:39:49 -0300

    Add querys in repositories

--- backend/src/main/java/com/mapin/docadmin/repositories/OfficeHoursRepository.java
+++ backend/src/main/java/com/mapin/docadmin/repositories/OfficeHoursRepository.java
@@ -1,11 +1,29 @@
 package com.mapin.docadmin.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.mapin.docadmin.entities.OfficeHours;
 
 @Repository
 public interface OfficeHoursRepository extends JpaRepository<OfficeHours, Long> {
+	
+	@Query("SELECT obj FROM OfficeHours obj WHERE obj.seg = true ")
+	Page<OfficeHours> findMonday(Pageable pageable);
+	
+	@Query("SELECT obj FROM OfficeHours obj WHERE obj.ter = true ")
+	Page<OfficeHours> findTuesday(Pageable pageable);
+	
+	@Query("SELECT obj FROM OfficeHours obj WHERE obj.qua = true ")
+	Page<OfficeHours> findWednesday(Pageable pageable);
+	
+	@Query("SELECT obj FROM OfficeHours obj WHERE obj.qui = true ")
+	Page<OfficeHours> findThursday(Pageable pageable);
+	
+	@Query("SELECT obj FROM OfficeHours obj WHERE obj.sex = true ")
+	Page<OfficeHours> findFriday(Pageable pageable);
 	
 }
--- backend/src/main/java/com/mapin/docadmin/repositories/SpecializationRepository.java
+++ backend/src/main/java/com/mapin/docadmin/repositories/SpecializationRepository.java
@@ -1,11 +1,17 @@
 package com.mapin.docadmin.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.mapin.docadmin.entities.Specialization;
 
 @Repository
 public interface SpecializationRepository extends JpaRepository<Specialization, Long> {
+	
+	@Query("SELECT obj FROM Specialization obj WHERE (LOWER(obj.name) LIKE LOWER(CONCAT('%',:name,'%'))) ")
+	Page<Specialization> find(String name, Pageable pageable);
 	
 }
